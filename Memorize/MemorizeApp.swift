e61d127cb1d10f7268ec27b64c7556dc93814e84
Claudia van den Boom <claudia@vdb-it.de>
2021-11-14 18:07:23 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Claudia on 14.11.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
