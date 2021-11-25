095950c9ad314a4084dc1a0b13947baf31c04759
Claudia van den Boom <claudia@vdb-it.de>
2021-11-25 13:54:04 +0100

    See the difference between a normal function call and a trailing closure.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
+/*
+ struct ContentView: View {
+     var body: some View {
+         ZStack(content: {
+             RoundedRectangle(cornerRadius: 50)
+                 .stroke(lineWidth: 3)
+
+                 .foregroundColor(/*@START_MENU_TOKEN@*/ .orange/*@END_MENU_TOKEN@*/)
+             Text("Hello Wolrd!")
+
+         })
+         .padding(.horizontal)
+         .foregroundColor(.red)
+     }
+ }
+ */
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 50)
+                .stroke(lineWidth: 3)
+                .foregroundColor(/*@START_MENU_TOKEN@*/ .orange/*@END_MENU_TOKEN@*/)
+
+            Text("Hello Wolrd!")
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
